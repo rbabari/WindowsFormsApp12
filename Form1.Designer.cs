95574ee075383eceefa87d7438a3159b77f071b5
rbabari <r.babari@gmail.com>
2020-02-19 08:46:43 -0500

    deplacer button
    deplacer button

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -56,7 +56,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(984, 188);
+            this.button2.Location = new System.Drawing.Point(984, 64);
             this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(112, 35);
